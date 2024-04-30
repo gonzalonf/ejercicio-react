0d24b7a0a8b0980c53c63f5544a006c351d16170
Gonzalo Fernandez <gonzalonf@gmail.com>
2024-04-30 13:28:38 -0300

    chore: remove unused code

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import React, { createContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const ModalContext = createContext(null);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ModalContext.Provider value={true}>
-      <App />
-    </ModalContext.Provider>
+    <App />
   </React.StrictMode>
 );
